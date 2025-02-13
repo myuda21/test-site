105a3314b03c1ab8644e465ec9b1db8b48f77cc8
Maxwell Yudanegara <maxwellyudanegara52@gmail.com>
2025-02-13 17:55:51 +0700

    feat: fix quality sleep shop folder formatting + change text font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,19 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        mulish: ['var(--font-mulish)', 'sans-serif'],
       },
     },
   },
