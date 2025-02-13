d11cf1d0b1f5eaa620f43a6dc6a11a33ddbad863
Maxwell Yudanegara <maxwellyudanegara52@gmail.com>
2025-02-13 11:17:30 +0700

    feat: fix navbar

--- app/home/navbar.tsx
+++ app/home/navbar.tsx
@@ -58,7 +58,7 @@ const Navbar = () => {
             </button>
             {isFeaturesOpen && (
               <div className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-lg py-2 z-50">
-                <Link href="/feature1" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Feature 1</Link>
+                <Link href="/archive" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Archive</Link>
                 <Link href="/feature2" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Feature 2</Link>
                 <Link href="/feature3" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Feature 3</Link>
               </div>
