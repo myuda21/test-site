1201f5111056995acec5534c28f53626af75c45a
Maxwell Yudanegara <maxwellyudanegara52@gmail.com>
2025-02-17 00:18:12 +0700

    Update productCard.tsx

--- src/app/(app)/mattresses/productCard.tsx
+++ src/app/(app)/mattresses/productCard.tsx
@@ -11,18 +11,20 @@ interface Product {
 
 const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
   return (
-    <div className="p-4">
-      <Image 
-        src={product.image} 
-        alt={product.name} 
-        width={300} 
-        height={200} 
-        className="object-cover"
-      />
-      <h2 className="text-lg font-semibold mt-2">{product.name}</h2>
-      <p className="text-gray-600">From ${product.price}</p>
-      <p className="text-yellow-500">⭐ {product.rating} ({product.reviews} Reviews)</p>
-    </div>
+    <a href="/product-details"> {/*TODO: ganti kalau dinamis*/}
+      <div className="p-4">
+        <Image
+          src={product.image}
+          alt={product.name}
+          width={300}
+          height={200}
+          className="object-cover"
+        />
+        <h2 className="text-lg font-semibold mt-2">{product.name}</h2>
+        <p className="text-gray-600">From ${product.price}</p>
+        <p className="text-yellow-500">⭐ {product.rating} ({product.reviews} Reviews)</p>
+      </div>
+    </a>
   );
 };
 
