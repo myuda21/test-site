9f3fcda2bce5d6c61d6f57145f2598946a34f206
Maxwell Yudanegara <maxwellyudanegara52@gmail.com>
2025-02-17 19:46:23 +0700

    feat: fix page + product page reviews

--- src/app/(app)/mattresses/productCard.tsx
+++ src/app/(app)/mattresses/productCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
 interface Product {
   id: number;
@@ -7,12 +8,31 @@ interface Product {
   image: string;
   rating: number;
   reviews: number;
+  filters?: string[];
 }
 
 const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
+  const renderStars = (rating: number) => {
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0;
+    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+    return (
+      <div className="flex space-x-1 mt-2">
+        {Array.from({ length: fullStars }, (_, i) => (
+          <FaStar key={`full-${i}`} className="text-yellow-500" />
+        ))}
+        {hasHalfStar && <FaStarHalfAlt className="text-yellow-500" />}
+        {Array.from({ length: emptyStars }, (_, i) => (
+          <FaRegStar key={`empty-${i}`} className="text-gray-300" />
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <a href="/product-details"> {/*TODO: ganti kalau dinamis*/}
-      <div className="p-4">
+    <a href="/product-details">
+      <div className="px-10 py-2">
         <Image
           src={product.image}
           alt={product.name}
@@ -20,9 +40,28 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
           height={200}
           className="object-cover"
         />
-        <h2 className="text-lg font-semibold mt-2">{product.name}</h2>
-        <p className="text-gray-600">From ${product.price}</p>
-        <p className="text-yellow-500">⭐ {product.rating} ({product.reviews} Reviews)</p>
+        <div className="flex justify-between">
+          <div>
+            <h2 className="text-lg font-semibold mt-2">{product.name}</h2>
+            <p className="text-gray-600">From ${product.price}</p>
+          </div>
+          <div>
+            {renderStars(product.rating)}
+            <p className="text-gray-500 mt-2">{product.reviews} Reviews</p>
+          </div>
+        </div>
+
+        {/* Features/Filters */}
+        <div className="flex flex-wrap mt-2">
+          {product.filters?.map((filter, index) => (
+            <div
+              key={index}
+              className="bg-gray-100 text-gray-600 px-3 py-1 rounded-md text-xs font-semibold mb-2 mr-2"
+            >
+              {filter}
+            </div>
+          ))}
+        </div>
       </div>
     </a>
   );
