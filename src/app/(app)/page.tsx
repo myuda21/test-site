e12af8ba78a3a91ece9c348c730075e3c39ee0d1
maxwell yudanegara <maxwellyudanegara52@gmail.com>
2025-04-14 12:55:36 +0700

    Update page.tsx

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,23 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      router.push("/home");
+    }, 2000); // 2 detik delay sebelum redirect
+
+    return () => clearTimeout(timeout);
+  }, [router]);
+
   return (
-    <Link href="/home">HOME</Link>
-  )
+    <div>
+      {loading && <p>Loading...</p>}
+    </div>
+  );
 }
