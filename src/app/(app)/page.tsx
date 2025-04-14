3d42cef85f41e191b82662a85a54a3765b9d394e
maxwell yudanegara <maxwellyudanegara52@gmail.com>
2025-04-14 13:48:09 +0700

    Update page.tsx

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -10,14 +10,14 @@ export default function Home() {
   useEffect(() => {
     const timeout = setTimeout(() => {
       router.push("/home");
-    }, 2000); // 2 detik delay sebelum redirect
+    }, 1000); // 2 detik delay sebelum redirect
 
     return () => clearTimeout(timeout);
   }, [router]);
 
   return (
-    <div>
-      {loading && <p>Loading...</p>}
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-gray-900"></div>
     </div>
   );
 }
